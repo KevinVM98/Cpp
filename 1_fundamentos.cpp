8afbdfdc4ed80c1de410ee66821badfef0b491b1
Kevin Vásquez Marín <93720836+KevinVM98@users.noreply.github.com>
2024-10-22 13:12:14 -0600

    Update 1_fundamentos.cpp

--- 1_fundamentos.cpp
+++ 1_fundamentos.cpp
@@ -1,7 +1,23 @@
+
+//Escribe un programa que calcule el área de un triángulo dado su base y altura.
+
 #include <iostream>
-using namespace std;
 
 int main() {
-    cout << "Hello, GitHub!" << endl;
+    float base, altura, area; //definimos el tipo de dato 
+
+    // Solicitar la base y la altura al usuario
+    
+    std::cout << "Introduce la base del triángulo: ";
+    std::cin >> base;
+    std::cout << "Introduce la altura del triángulo: ";
+    std::cin >> altura;
+
+    // Calcular el área del triángulo
+    area = (base * altura) / 2;
+
+    // Mostrar el resultado
+    std::cout << "El área del triángulo es: " << area << std::endl;
+
     return 0;
 }
